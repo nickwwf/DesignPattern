93635345a04a81d7e60b0c36cc929a7172933123
wwf <wwf@swift.ac>
2022-03-21 13:44:23 +0800

    fib

--- 重写Equals/Program.cs
+++ 重写Equals/Program.cs
@@ -6,12 +6,40 @@ namespace 重写Equals
     {
         static void Main(string[] args)
         {
-            var t1 = new Test() { MyProperty = "1" };
-            var t2 = new Test() { MyProperty = "1" };
-            string str1 = "aa";
-            string str2 = "aa";
-            Console.WriteLine(str1.Equals(str2));
+            //var t1 = new Test() { MyProperty = "1" };
+            //var t2 = new Test() { MyProperty = "1" };
+            //string str1 = "aa";
+            //string str2 = "aa";
+            //Console.WriteLine(str1.Equals(str2));
+            //Console.ReadKey();
+
+            Console.WriteLine(GetNumber(10));
             Console.ReadKey();
+        }
+
+        //输出斐波那契额数列
+        private static void FibTest()
+        {
+            Func<int, int> Fib = num =>
+            {
+                var a = 1;
+                var b = 1;
+                for (var i = 0; i < num - 1; i++)
+                {
+                    var t = a;
+                    a = b;
+                    b = a + t;
+                }
+                return a;
+            };
+
+            Console.WriteLine(Fib(10).ToString());
+        }
+
+        private static int GetNumber(int num)
+        {
+            if (num == 1 || num == 2) return 1;
+            return GetNumber(num - 1) + GetNumber(num - 2);
         }
     }
 
